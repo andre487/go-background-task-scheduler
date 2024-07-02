2b0be4651c003684ddda51627079dce0f5f1ff1c
Andrey Prokopyuk <andrey.prokopyuk@gmail.com>
2024-07-02 08:32:47 +0300

    Improve docs for err.go

--- err.go
+++ err.go
@@ -2,7 +2,7 @@ package bgscheduler
 
 import "errors"
 
-// SchedulerError is the instance of error that identifies this package errors.
+// SchedulerError is the instance of error that identifies this package internal errors.
 // SchedulerError is in the chains of all errors returned from this package
 // excluding errors from Task, they returned as is.
 var SchedulerError = errors.New("BG scheduler error")
